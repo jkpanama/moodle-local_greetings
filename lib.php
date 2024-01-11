502d34b0ac019f9085f366bb9781793a3d0c9412
jkpanama <jk_panama@outlook.com>
2024-01-11 11:47:53 -0500

    LIB

--- lib.php
+++ lib.php
@@ -15,25 +15,28 @@
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
+ * Plugin strings are defined here.
+ *
  * @package     local_greetings
- * @copyright   2023 Karina Panama <karina@example.com>
+ * @category    string
+ * @copyright   2024 Karina Panam<karina@example.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once('../../config.php');
-require_once($CFG->dirroot. '/local/greetings/lib.php');
+function local_greetings_get_greeting($user) {
+    if ($user == null) {
+        return get_string('greetinguser', 'local_greetings');
+    }
 
-$context = context_system::instance();
-$PAGE->set_context($context);
-$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
-$PAGE->set_title($SITE->fullname);
-$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
-$PAGE->set_pagelayout('standard');
+    $country = $user->country;
+    switch ($country) {
+        case 'ES':
+            $langstr = 'greetinguseres';
+            break;
+        default:
+            $langstr = 'greetingloggedinuser';
+            break;
+    }
 
-echo $OUTPUT->header();
-if (isloggedin()) {
-    echo local_greetings_get_greeting($USER);
-} else {
-    echo get_string('greetinguser', 'local_greetings');
+    return get_string($langstr, 'local_greetings', fullname($user));
 }
-echo $OUTPUT->footer();
